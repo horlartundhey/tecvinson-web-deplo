d78d6b8645c9acf7a52f99b0f7887750b52a7bc4
olatunde <horlartundhey@gmail.com>
2025-02-25 11:49:18 +0100

    removed the legacy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
-
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   plugins: [
-    react(),  
-    svgrPlugin()
+    react(),
+    svgrPlugin(),
   ],
   optimizeDeps: {
     include: ["framer-motion"],
